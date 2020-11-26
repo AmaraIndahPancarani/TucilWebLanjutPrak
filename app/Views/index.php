c09efd9ba07107d7d62859c1cce425caf0219779
AmaraIndahPancarani <amaraindah24@gmail.com>
2020-11-27 06:39:17 +0700

    TucilWebLanjut

---
+++ app/Views/index.php
@@ -0,0 +1,54 @@
+</head>
+
+
+<div class="jumbotron jumbotron-fluid mb-5">
+    <div class="container text-center">
+        <h1 class="display-4">Reference women's sports equipment.</h1>
+    </div>
+</div>
+
+<div class="content" id="content">
+    <div class="container">
+        <div class="row justify-content-center mb-5">
+            <div class="col-md-4">
+                <img src="<?= base_url('assets'); ?>/3.jpg" width="100%" class="image">
+            </div>
+            <div class="col-md-5">
+                <h3>[FAVORITE] RUNNING</h3>
+                <br>
+                <p>Running is actually a great way to increase your overall level of health. Research shows that running can raise your levels of good cholesterol while also helping you increase lung function and use...</p>
+                <a type="button" class="btn btn-light" href="<?= base_url('Home/running'); ?>">DETAIL</a>
+            </div>
+        </div>
+
+        <div class="row justify-content-center mb-5">
+            <div class="col-md-4">
+                <img src="<?= base_url('assets'); ?>/4.jpg" width="100%" class="image">
+            </div>
+            <div class="col-md-5">
+                <h3>TENNIS</h3>
+                <br>
+                <p>Tennis is a racket sport that can be played individually against a single opponent (singles) or between two teams of two players each (doubles)....</p>
+                <a type="button" class="btn btn-light">DETAIL</a>
+            </div>
+        </div>
+
+        <div class="row justify-content-center mb-5">
+            <div class="col-md-4">
+                <img src="<?= base_url('assets'); ?>/5.jpg" width="100%" class="image">
+            </div>
+            <div class="col-md-5">
+                <h3>PLAYING FOOTBALL</h3>
+                <br>
+                <p>Many women have athletic success in individual sports, but athletes who have played team sports will tell you that there’s nothing quite like being part of a team! Women’s football gives athletes a chance to be part of something bigger than themselves and to play alongside other great female athletes....</p>
+                <a type="button" class="btn btn-light">DETAIL</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<hr>
+
+<div class="content text-center">
+    <button type="button" class="btn btn-link text-center">MORE REFERENCE</button>
+</div>
---
+++ app/Views/running.php
@@ -0,0 +1,39 @@
+</head>
+
+<div class="jumbotron jumbotron-fluid mb-5">
+    <div class="container text-center">
+        <h1 class="display-4">Running is Best Reference for Women.</h1>
+    </div>
+</div>
+
+<section id="content" class="content">
+    <div class="container justify-content-center col-8">
+        <h2 class="text-center mb-5">[FAVORITE] RUNNING</h2>
+
+        <p>For women, running can actually help to lower your risk of breast cancer. It can also help reduce the risk of having a stroke. Many doctors today recommend running for people who are in the early stages of diabetes, high blood pressure, and osteoporosis, and it is proven to help reduce the risk of having a heart attack. By helping the arteries retain their elasticity and strengthening the heart, your chances of suffering a heart attack can be significantly reduced.</p>
+        <div class="mb-4">
+            <img src="<?= base_url('assets'); ?>/7.jpg" width="100%">
+            <h6>Source: Google</h6>
+        </div>
+
+        <p>Running is one of the best forms of exercise for losing or maintaining a consistent weight. You will find that it is a leading way to burn off extra calories and that it is the second most effective exercise in terms of calories burned per minute, following only after cross country skiing.</p>
+        <p>Not all of the benefits of running are physical. Running can provide an noticeable boost to your confidence and self-esteem. By setting and achieving goals, you can help give yourself a greater sense of empowerment that will leave you feeling much happier.</p>
+
+        <div class="mb-4">
+            <img src="<?= base_url('assets'); ?>/8.jpg" width="100%">
+            <h6>Source: Google</h6>
+        </div>
+
+        <p>Stress can actually cause a number of health and mood problems. It can also diminish appetite and sleep quality. When you run, you force your body to exert excess energy and hormones. Running also helps to reduce your chances of developing tension headaches.</p>
+
+        <div class="mb-4">
+            <img src="<?= base_url('assets'); ?>/9.jpg" width="100%">
+            <h6>Source: Google</h6>
+        </div>
+
+        <p>When you are depressed, the last thing you likely want to do is to get up and go for a run. Yet you will find that after only a few minutes of running, your brain will start to secrete hormones that naturally improve your mood. In fact, there are few things in the world that can better or more rapidly treat depression than exercise such as running.</p>
+        <p>Running is incredibly beneficial to the body, mind and spirit. You will find that even short runs can leave you feeling more energized, more focused and better able to enjoy all that life has to offer.</p>
+        <br>
+        <h6 class="text-right">by https://www.active.com/running/articles/6-benefits-of-running</h6>
+    </div>
+</section>
